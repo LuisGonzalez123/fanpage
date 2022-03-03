47483cd90dc9385e2bf860522096d804c430f4f1
shainluis <82976658+shainluis@users.noreply.github.com>
2022-03-03 00:02:06 -0500

    update

---
+++ lib/helper/post.dart
@@ -0,0 +1,6 @@
+class Post {
+  final String id;
+  final String message;
+
+  const Post({required this.id, required this.message});
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,53 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
-// class HomePage extends StatelessWidget {
-//   HomePage({Key? key}) : sper(key : key);
-//   FirebaseFirestore _db = FirebaseFirestore.instance;
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  FirebaseFirestore _db = FirebaseFirestore.instance;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder(
-      
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _db.collection("Post").snapshots(),
+      builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+        return Container();
+      },
+    );
+  }
+}
+
+class HomePage2 extends StatefulWidget {
+  HomePage2({Key? key}) : super(key: key);
+
+  State<HomePage2> createState() => Home();
+}
+
+class Home extends State<HomePage2> {
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+  @override
+  void initState() async {
+    super.initState();
+    var snapshots = await _db.collection("posts").snapshots();
+    snapshots.forEach((element) {
+      element.docs.forEach((doc) {
+        doc.data();
+      });
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return StreamBuilder(
+        stream: _db.collection("Post").snapshots(),
+        builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return Container();
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
